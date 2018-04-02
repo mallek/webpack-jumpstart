9d0a60c7028f76d644c3e9b36c8a5dfc83011aef
mallek <travis@haleycomputersolutions.com>
2018-04-02 04:35:19 -0600

    added babel env preset

--- src/main.js
+++ src/main.js
@@ -1,7 +1,11 @@
+require("babel-runtime/regenerator")
 require("./main.css")
 require("./index.html")
 
-var a = async() => {
+var a = async(args) => {
+    const { a, b } = args
     await console.log("hello from the future!")
     console.log("done")
 }
+
+a({ a: 1, b: 2 })
