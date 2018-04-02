352e95f26e39fee5e8eca73fb80508139f0c73f0
mallek <travis@haleycomputersolutions.com>
2018-04-02 04:54:55 -0600

    added express dev server and webpack middleware

--- config/webpack.dev.js
+++ config/webpack.dev.js
@@ -1,4 +1,5 @@
 const path = require("path")
+const webpack = require("webpack")
 
 module.exports = {
     entry: {
@@ -12,7 +13,11 @@ module.exports = {
     },
     devServer: {
         contentBase: "dist",
-        overlay: true
+        overlay: true,
+        hot: true,
+        stats: {
+            colors: true
+        }
     },
     module: {
         rules: [{
@@ -61,6 +66,9 @@ module.exports = {
                 }]
             }
         ]
-    }
+    },
+    plugins: [
+        new webpack.HotModuleReplacementPlugin()
+    ]
 
 }
--- src/main.js
+++ src/main.js
@@ -1,11 +1,4 @@
 require("babel-runtime/regenerator")
+require("webpack-hot-middleware/client")
 require("./main.css")
 require("./index.html")
-
-var a = async(args) => {
-    const { a, b } = args
-    await console.log("hello from the future!")
-    console.log("done")
-}
-
-a({ a: 1, b: 2 })
