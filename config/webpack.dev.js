1db8eaf1d2079ba9cb0491e6daa3316987aa259d
mallek <travis@haleycomputersolutions.com>
2018-04-14 12:59:09 -0600

    typescript loader

--- config/webpack.dev.js
+++ config/webpack.dev.js
@@ -4,7 +4,8 @@ const HTMLWebpackPlugin = require("html-webpack-plugin")
 
 module.exports = {
     entry: {
-        main: ["./src/main.js"]
+        main: ["./src/main.js"],
+        ts: ["./src/index.ts"]
     },
     mode: "development",
     output: {
@@ -22,10 +23,18 @@ module.exports = {
     },
     devtool: "source-map",
     module: {
-        rules: [{
+        rules: [
+            {
                 test: /\.js$/,
                 use: [{
                     loader: "babel-loader"
+                }],
+                exclude: /node_modules/
+            },
+            {
+                test: /\.ts$/,
+                use: [{
+                    loader: "awesome-typescript-loader"
                 }],
                 exclude: /node_modules/
             },
